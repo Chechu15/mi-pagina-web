459d02d4a1e9d48361be11821f9913d5034c9a29
Master <master@pm.com>
2025-05-19 17:50:16 +0200

    Incorporacion del Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,17 @@
+// src/components/Footer.tsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto px-4 text-center">
+       
+        <div>
+          <p>&copy; 2025 Mi Página Web. Todos los derechos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -39,6 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         </main>
         <ScrollRestoration />
         <Scripts />
+        <Footer />
       </body>
     </html>
   );
