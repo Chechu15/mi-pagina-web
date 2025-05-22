d3eaf518c69ffef72c3aeb959736f88284da72c6
Master <master@pm.com>
2025-05-22 15:18:26 +0200

    Comentando footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,16 @@
 // src/components/Footer.tsx
 import React from 'react';
 
+// Componente Footer que muestra el pie de página de la aplicación
+
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto px-4 text-center">
+           {/* Contenedor que contiene el texto del pie de página */}
        
         <div>
+               {/* Texto del año y derechos de autor */}
           <p>&copy; 2025 Mi Página Web. Todos los derechos reservados.</p>
         </div>
       </div>
