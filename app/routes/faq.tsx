460848f8562737afee5e38ce49a7e8f3f449ae84
Master <master@pm.com>
2025-05-24 13:34:34 +0200

    Argumentando el faq

--- app/routes/faq.tsx
+++ app/routes/faq.tsx
@@ -16,21 +16,24 @@ export default function FAQ() {
       </h1>
       <div className="space-y-4">
         <div>
-          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Pregunta 1</h2>
+          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">¿Que distribución escoger para el uso cotidiano?</h2>
           <p className="text-lg text-gray-600 dark:text-gray-300">
-            Respuesta 1
+            Para el uso cotidiano recomendamos distribuciones como Ubuntu, Linux Mint o Fedora
           </p>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Pregunta 2</h2>
+          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">¿Que distribución escoger para crear un servidor?</h2>
           <p className="text-lg text-gray-600 dark:text-gray-300">
-            Respuesta 2
+            Para crear un servidor depende de las necesidades y la infraesctructura que vayas a escoger , para algo sencillo recomendamos Alpine Linux, en cambio 
+            si requieres de un SO mas potente recomendamos Ubuntu Server o centOS y para un uso empreasarial RedHat
           </p>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Pregunta 3</h2>
+          <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Si soy un punto de mira de países u organismos oficiales , que distribución escojo?</h2>
           <p className="text-lg text-gray-600 dark:text-gray-300">
-            Respuesta 3
+            Para una persona que se tenga que esconder de gente muy poderosa y que si comete algún error le pueda costar la vida. Recomendamos SO como 
+            QubesOS o un sistema que no deja huella en ningún ordenador como Tails
+  
           </p>
         </div>
       </div>
